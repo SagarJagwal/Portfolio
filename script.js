73f2f98d3769c9d15db02ff6fe7ba71d585b0ddb
Sagarjagwal-GD <jagwal.sagar@intern1.gravitechdreams.com>
2024-02-11 22:49:25 +0530

    My Portfolio

---
+++ script.js
@@ -0,0 +1,68 @@
+let navlinks = document.querySelectorAll('header ul li');
+
+navlinks.forEach(nav => {
+    nav.addEventListener('click',function(){
+        navlinks.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        document.getElementById('click').checked = false;
+    });
+});
+
+
+// project cards scrolling code here =================================
+
+const projectSlider = document.querySelector(".skills-project-wrapper");
+
+let pressed = false;
+let startX = 0;
+
+projectSlider.addEventListener('mousedown', function (e) {
+    pressed = true;
+    startX = e.clientX;
+})
+
+projectSlider.addEventListener('mouseleave', function (e) {
+    pressed = false;
+})
+
+projectSlider.addEventListener('mousemove', function (e) {
+    if(!pressed){
+        return;
+    }
+    this.scrollLeft += startX - e.clientX;
+})
+
+window.addEventListener('mouseup', function (e) {
+    pressed = false;
+})
+
+// ============================== HTML to GSheet =================================
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbySAtK6YK-CGEhsNycXTvPTrTxey9ic04hpZazpZBcH50FX84Rfzuzv2x93R5xyu38x/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    document.getElementById("submit-btn").value = "Please Wait";
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => {
+    alert("Thank you! your massege sent successfully." )})
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
+
+//===================  Download Links  ==========================
+
+function visit(num){
+    switch(num){
+        case 1 : 
+            window.open("https://drive.google.com/file/d/1-CzlJ0lZ8RQOZeZoOkvt06rdEQYZGgk7/view?usp=sharing","_blank");
+        case 2 : 
+            window.open("https://drive.google.com/file/d/1LMRiM5dsAsLed5gP2vaQNi2PO8-qo1f-/view?usp=sharing","_blank");
+        case 3 : 
+            window.open("https://drive.google.com/drive/folders/1T5JU57e4UDy2xa991abOfdUTsp5DHvCA?usp=drive_link","_blank");
+        case 4 : 
+            window.open("https://drive.google.com/drive/folders/1T5NK2Z3T_F1VNW8WHQFmj9Br_wiSa0go?usp=drive_link","_blank");
+    }
+}
